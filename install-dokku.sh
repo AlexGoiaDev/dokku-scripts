df76b700a5e6c50cadf2aba35f991581e9034c75
Alex Goia <alexgoiadev@gmail.com>
2022-10-09 14:14:51 +0200

    feat: uncoment commads

--- install-dokku.sh
+++ install-dokku.sh
@@ -19,20 +19,20 @@ echo "${C_STEELBLUE1}DOMAIN:${NO_FORMAT} $1"
 echo "${C_STEELBLUE1}EMAIL (letsencrypt):${NO_FORMAT} $2"
 echo "Initial server settings with Dokku"
 echo "1. Obtaining bash..."
-# wget https://raw.githubusercontent.com/dokku/dokku/v0.28.1/bootstrap.sh
+wget https://raw.githubusercontent.com/dokku/dokku/v0.28.1/bootstrap.sh
 echo "${C_SPRINGGREEN2}OK, i got that bash :P${NO_FORMAT}"
 echo "2. Executing bash:"
-# sudo DOKKU_TAG=v0.28.1 bash bootstrap.sh
+sudo DOKKU_TAG=v0.28.1 bash bootstrap.sh
 echo "${C_SPRINGGREEN2}GREAT, I installed that bash!${NO_FORMAT}"
 echo "3. Asociating ssh-key with admin name:"
-# cat ~/.ssh/authorized_keys | sudo dokku ssh-keys:add admin
+cat ~/.ssh/authorized_keys | sudo dokku ssh-keys:add admin
 echo "${C_SPRINGGREEN2}SSH KEY asociaed! :)${NO_FORMAT}"
 echo "4. Setting global domain to ${1}"
-# dokku domains:set-global $1
+dokku domains:set-global $1
 echo "${C_SPRINGGREEN2} Domain running great!${NO_FORMAT}"
 echo "5. Adding letsencrypt plugin:"
-# sudo dokku plugin:install https://github.com/dokku/dokku-letsencrypt.git
+sudo dokku plugin:install https://github.com/dokku/dokku-letsencrypt.git
 echo "6. Setting letsencrypt email to ${2}"
-# dokku config:set --global DOKKU_LETSENCRYPT_EMAIL=emaildelclient@dominiodelcliente.com
-# dokku letsencrypt:cron-job --add
+dokku config:set --global DOKKU_LETSENCRYPT_EMAIL=emaildelclient@dominiodelcliente.com
+dokku letsencrypt:cron-job --add
 echo "Summary: ${C_STEELBLUE1}https://$1 ${NO_FORMAT}encrypted with letsencrypt(${C_STEELBLUE1}$2${NO_FORMAT})"

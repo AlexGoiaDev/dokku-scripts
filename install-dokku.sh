4f23b8e4c8123224ee5d97ee1e678e95e09575c8
Alex Goia <alexgoiadev@gmail.com>
2022-10-09 14:04:07 +0200

    feat: remove new lines

--- install-dokku.sh
+++ install-dokku.sh
@@ -7,20 +7,16 @@ fi
 
 if [ -z "$2" ]
   then
-    echo Required letsencrypt email, examp yourfancyemail@gmail.com
+    echo "Required letsencrypt email, examp yourfancyemail@gmail.com"
     exit 1
 fi
-
 NO_FORMAT="\033[0m"
 C_SPRINGGREEN2="\033[38;5;47m"
 C_STEELBLUE1="\033[38;5;75m"
 C_ORANGE1="\033[38;5;214m"
-
-
 echo "${C_ORANGE1}BASH CREATED BY TWISS${NO_FORMAT}"
 echo "${C_STEELBLUE1}DOMAIN:${NO_FORMAT} $1"
 echo "${C_STEELBLUE1}EMAIL (letsencrypt):${NO_FORMAT} $2"
-
 echo "Initial server settings with Dokku"
 echo "1. Obtaining bash..."
 # wget https://raw.githubusercontent.com/dokku/dokku/v0.28.1/bootstrap.sh
@@ -34,16 +30,9 @@ echo "${C_SPRINGGREEN2}SSH KEY asociaed! :)${NO_FORMAT}"
 echo "4. Setting global domain to ${1}"
 # dokku domains:set-global $1
 echo "${C_SPRINGGREEN2} Domain running great!${NO_FORMAT}"
-
 echo "5. Adding letsencrypt plugin:"
 # sudo dokku plugin:install https://github.com/dokku/dokku-letsencrypt.git
 echo "6. Setting letsencrypt email to ${2}"
 # dokku config:set --global DOKKU_LETSENCRYPT_EMAIL=emaildelclient@dominiodelcliente.com
 # dokku letsencrypt:cron-job --add
-
-
 echo "Summary: ${C_STEELBLUE1}https://$1 ${NO_FORMAT}encrypted with letsencrypt(${C_STEELBLUE1}$2${NO_FORMAT})"
-
-
-
-
